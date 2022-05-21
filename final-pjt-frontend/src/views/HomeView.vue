f2399e34698e0afd967c9cda510612d2b786a310
harvey <harveydev24@gmail.com>
2022-05-21 14:05:46 +0900

    Movie API

--- final-pjt-frontend/src/views/HomeView.vue
+++ final-pjt-frontend/src/views/HomeView.vue
@@ -5,10 +5,19 @@
 </template>
 
 <script>
+// function getYesterday() {
+//   var date = new Date();
+//   var year = date.getFullYear();
+//   var month = ("0" + (1 + date.getMonth())).slice(-2);
+//   var day = ("0" + (date.getDate() - 1)).slice(-2);
+
+//   return year + month + day;
+// }
 
 export default {
-  name: 'HomeView',
-  components: {
-  }
-}
+  name: "HomeView",
+  components: {},
+
+  created() {},
+};
 </script>
